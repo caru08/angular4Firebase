54525b996686630be95ec137ef1df7abe34f8a0f
Carina Muñoz <carinamunoz@suremptec.com.ar>
2018-05-02 17:31:16 -0300

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  email = "";
+  pass = "";
+  loggedUser: any = {};
+
+  constructor(public afAuth: AngularFireAuth, public afDataBase: AngularFireDatabase){
+    this.afAuth.authState.subscribe((response)=>{
+      console.log("se chequeo el logueo", response);
+      this.loggedUser = response ? response : {};
+    }, (error)=>{
+      console.log("error al chequear logueo", error);
+    })
+  }
+
+  login(){
+    this.afAuth.auth.signInWithEmailAndPassword(this.email, this.pass)
+     .then((response) => {
+      console.log("se logueo correctamente");
+      //cada vez que se logueo o desloguea entra al subscribe del constructor
+     })
+     .catch(error => console.log(error));     
+  }
+
+  gmailLoggedIn(){
+      this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then(response => {
+      console.log("se logueo correctamente. usuario:", response);
+    })
+    .catch(error => {console.log("error al loguearse con google")});
+  }
+
+  registrarse(){
+    debugger;
+    this.afAuth.auth.createUserWithEmailAndPassword(this.email, this.pass)
+     .then((response) => {
+       console.log("se registro correctamente. usuario:", response);
+       //cada vez que se logueo o desloguea entra al subscribe del constructor
+    //   this.loggedUser = response;
+     })
+     .catch(error => console.log("error al registrarse",error));
+  }
+
+  logout() {
+    this.afAuth.auth.signOut()
+    .then(response => {
+      console.log("se deslogueo correctamente. respuesta:", response);
+      this.loggedUser = {};
+    })
+    .catch(error => console.log("error al desloguearse", error));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { ImplicitReceiver } from '@angular/compiler';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4M7hnrIGdpb3ETAYWcZNU8Xcclyvu244",
+  authDomain: "practicaclase1-592dd.firebaseapp.com",
+  databaseURL: "https://practicaclase1-592dd.firebaseio.com",
+  projectId: "practicaclase1-592dd",
+  storageBucket: "practicaclase1-592dd.appspot.com",
+  messagingSenderId: "148388018465"
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
